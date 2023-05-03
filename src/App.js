1041b7bf9569d0ea84c6747a9a11220819c3dfb9
gitbhanu7 <bhanu.git2107@gmail.com>
2023-05-03 21:30:52 +0530

    'Initial'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState ,  useEffect } from 'react';
+import axios from 'axios'
+
+
+
 
 function App() {
+
+  const [values , setValues ] =  useState({
+    name : "",
+    email  :""
+  })
+
+  const [data , setData] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:8990/get-data').then((res)=>{
+      console.log(res)
+
+      setData(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  },[data])
+
+  const handleInputs =  (e) =>{
+
+    setValues({...values , [e.target.name] : e.target.value })
+
+  }
+
+  const submit =  ()=>{
+    console.log(values)
+    axios.get('http://localhost:8990/add-data' , {params: values}).then((res)=>{
+      console.log(res)
+      alert(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+    <input name='name' value={values.name} onChange={handleInputs} placeholder='Enter Name'  />
+    <input name="email" value={values.email}  onChange={handleInputs}  placeholder='Enter Email' />
+    <button onClick={submit} >Add</button>
+
+    {data.map((el,i)=>(
+      <h1> {i+1} {el.name} {el.email}</h1>
+    ))}
+    
+</>
   );
 }
 
